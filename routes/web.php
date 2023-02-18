a6cd759dc0e6d56dd28d7a235a6a3b6259848321
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-18 18:49:35 +0100

    Episodes 30: Eager Load Relationships on an Existing Model

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
    $posts = Post::all();
 
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
