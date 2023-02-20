074ba9604b1d441da1fedc3ff1aa1ab3bd1a3677
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-20 15:34:04 +0100

    Episode 47: Failed Validation and Old Input Data

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,9 +14,9 @@ class RegisterController extends Controller
     public function store(){
 
         $attributes = request()->validate([
-            'name' => 'required',
-            'username'=> 'required|max:255',
-            'email' => 'required|email',
+            'name' => 'required|min:5',
+            'username'=> 'required|max:255|min:3|unique:users,username',
+            'email' => 'required|email|unique:users,email',
             'password' => ['required', 'max:255', 'min:5']
         ]);
 
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -14,8 +14,12 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{old('name')}}"
                            required
                     >
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{!! $message !!} </p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -28,8 +32,12 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{old('username')}}"
                            required
                            >
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{!! $message !!} </p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -42,8 +50,12 @@
                            type="text"
                            name="email"
                            id="email"
+                           value="{{old('email')}}"
                            required
                     >
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{!! $message !!} </p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -56,8 +68,12 @@
                            type="password"
                            name="password"
                            id="password"
+                           value="{{old('password')}}"
                            required
                     >
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{!! $message !!} </p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
