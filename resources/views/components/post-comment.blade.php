3c8efd9a02bbf9f6a8ca802e7307774d2e12f769
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-21 10:37:01 +0100

    Episode 52: Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<article class="flex bg-gray-100 p-6 border border-gray rounded-xl space-x-4">
+    <div>
+        <img src="https://i.pravatar.cc/100" width="60"/>
+    </div>
+
+    <div>
+        <header>
+            <h3 class="font-bold">Jerome Kwetters</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+    </div>
+</article>
