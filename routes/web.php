638f2a3e3a68599e4f9db7df243e7ef51d9ac6f8
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-22 13:53:16 +0100

    Episode 58

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;
@@ -7,6 +8,22 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentController;
+use MailchimpMarketing\ApiClient;
+
+
+Route::get('ping', function(){
+    $mailchimp = new ApiClient();
+
+    $mailchimp->setConfig([
+        'apikey' => 'acf6e35352dd1036669b41ccf811ff60-us21',
+        'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->ping->get();
+    //dd($response);
+    return('hello!');
+
+});
 
 
 Route::get('/', [PostController::class, 'index'])->name('home');
